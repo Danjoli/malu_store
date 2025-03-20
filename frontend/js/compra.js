cef57980443a743a10cee605a1331283b2ba8425
Danjoli <danjoli50@gmail.com>
2025-03-20 20:16:14 -0300

    simplifiquei a redundancia do botao descricao

--- frontend/js/compra.js
+++ frontend/js/compra.js
@@ -38,7 +38,21 @@ fetch('../produtos.json')
     .catch(error => console.error('Erro ao carregar os produtos:', error));
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const btnDescricao = document.getElementById("btnDescricao");
+    const descricaoTexto = document.getElementById("produto-descricao");
 
+    btnDescricao.addEventListener("click", function () {
+        if (descricaoTexto.style.display === "none" || descricaoTexto.style.display === "") {
+            descricaoTexto.style.display = "block";  // Exibe a descrição
+            btnDescricao.textContent = "-";          // Muda o botão para "-"
+        } else {
+            descricaoTexto.style.display = "none"    // Oculta a descrição
+            btnDescricao.textContent = "+";          // Muda o botão para "+"
+        }
+    })
+})
+    
 
 
 
