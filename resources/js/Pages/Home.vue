30d230ddf5cb959ba252ffa1c790f84e48a6fc44
Danjoli <danjoli50@gmail.com>
2026-01-13 16:03:39 -0300

    feat: add first Inertia page and Home route

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    message: String
+})
+</script>
+
+<template>
+    <div class="p-8">
+        <h1 class="text-3xl font-bold text-red-800">
+            {{ message }}
+        </h1>
+
+        <p class="mt-4 text-gray-600">
+            Laravel + Inertia + Vue funcionando corretamente 🚀
+        </p>
+    </div>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <title>Malu Store</title>
+
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+<body class="font-sans antialiased">
+    @inertia
+</body>
+</html>
