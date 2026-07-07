df93b184cc6eae5818f1f23ef97b75c01f436ee9
Danjoli <danjoli50@gmail.com>
2026-07-07 12:04:35 -0300

    Corrigindo api mercado pago

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,11 @@ import './products/variants';
 
 import './dashboard/index';
 
+// Pagamentos
+import './payments/pix';
+import './payments/card';
+import './payments/boleto';
+
 import Alpine from 'alpinejs';
 import Swal from 'sweetalert2';
 
